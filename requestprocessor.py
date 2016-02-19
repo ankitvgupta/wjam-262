8a00d7c5109e23e660d3d3565a5f154222c107ac
Matthew Rauen <rauen@college.harvard.edu>
2016-02-19 18:44:12 -0500

    Work on API

--- requestprocessor.py
+++ requestprocessor.py
@@ -1,30 +1,40 @@
 class RequestProcessor:
 
-  def __init__(self):
-    # userId -> user object
-    users    = {}
-    # groupId -> list of userIds
-    groups   = {}
-    # userId -> pending messages
-    messages = {}
+    def __init__(self):
+      # userId -> user object
+      self.users    = {}
+      # groupId -> list of userIds
+      self.groups   = {}
+      # userId -> pending messages
+      self.messages = {}
 
-  def register_user(self, request_object):
-      pass
+    def register_user(self, request_object):
+        if len(self.users) == 0:
+          next_id = 0
+        else:
+          next_id = max(self.users) + 1
+        self.users[next_id] = { "username" : request_object["username"], "password" : request_object["password"] }
+        return "Success!"
 
-  def list_accounts(self, request_object):
-      pass
+    def list_accounts(self, request_object):
+        pass
 
-  def create_group(self, request_object):
-      pass
+    def create_group(self, request_object):
+        if len(self.groups) == 0:
+          next_id = 0
+        else:
+          next_id = max(self.groups) + 1
+        self.groups[next_id] = { "name" : request_object["name"], "users" : request_object["users"] }
+        return "Success!"
 
-  def list_groups(self, request_object):
-      pass
+    def list_groups(self, request_object):
+        pass
 
-  def send_message(self, request_object):
-      pass
+    def send_message(self, request_object):
+        pass
 
-  def get_messages(self, request_object):
-      pass
+    def get_messages(self, request_object):
+        pass
 
-  def delete_account(self, request_object):
-      pass
+    def delete_account(self, request_object):
+        pass
--- server.py
+++ server.py
@@ -3,38 +3,38 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
+if sys.argv[2] == 'rest':
+    stub = RestAPIStub()
+else:
+    stub = WireProtocolStub()
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+# Initialize stub
+# Initialize processor
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    print request
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': tasks}), 201
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    request_object = stub.decode(path, request)
+    #if request_object["type"] == TYPEA
+    #    request_processor.TYPEA()
+
+# @app.route("/")
+# def hello():
+#     return render_template('index.html')
+
+# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+# def create_task():
+#     print request
+#     if not request.json or not 'title' in request.json:
+#         abort(400)
+#     task = {
+#         'id': tasks[-1]['id'] + 1,
+#         'title': request.json['title'],
+#         'description': request.json.get('description', ""),
+#         'done': False
+#     }
+#     tasks.append(task)
+#     return jsonify({'task': tasks}), 201
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
