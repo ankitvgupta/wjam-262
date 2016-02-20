0f07e44322e9760555740578e37debf257786cd3
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-02-19 20:12:42 -0500

    updated naming

--- RestAPIStub.py
+++ RestAPIStub.py
@@ -8,22 +8,7 @@ class RestAPIStub:
 		target = path_values[0]
 		return_object = {}
 		assert(request.json)
-		if path_values[0] == "users" and path_values[1] == "create":
-			return_object["username"] = request.json['username']
-			return_object["password"] = request.json['password']
-			return_object["task"] = "CreateUser"
-			return return_object
-		elif path_values[0] == "users" and path_values[1] == "list":
-			return_object["username"] = request.json['username']
-			return_object["password"] = request.json['password']
-			return_object["task"] = "ListAccounts"
-			return return_object
-		elif path_values[0] == "users" and path_values[1] == "delete":
-			return_object["username"] = request.json['username']
-			return_object["password"] = request.json['password']
-			return_object["task"] = "DeleteAccount"
-			return return_object
-		elif path_values[0] == "users" and path_values[2] == "messages" and request.method == "GET":
+		if path_values[0] == "users" and path_values[2] == "messages" and request.method == "GET":
 			return_object["username"] = path_values[1]
 			return_object["password"] = request.json["password"]
 			return_object["task"] = "GetMessages"
@@ -35,12 +20,27 @@ class RestAPIStub:
 			return_object["targetgroup"] = request.json["targetgroup"]
 			return_object["task"] = "SendMessage"
 			return return_object
-		elif path_values[0] == "groups" and path_values[1] == "list":
+		elif path_values[0] == "users" and request.method == "GET":
+			return_object["username"] = request.json['username']
+			return_object["password"] = request.json['password']
+			return_object["task"] = "ListAccounts"
+			return return_object
+		elif path_values[0] == "users" and request.method == "DELETE":
+			return_object["username"] = request.json['username']
+			return_object["password"] = request.json['password']
+			return_object["task"] = "DeleteAccount"
+			return return_object
+		elif path_values[0] == "users" and request.method == "POST":
+			return_object["username"] = request.json['username']
+			return_object["password"] = request.json['password']
+			return_object["task"] = "CreateUser"
+			return return_object
+		elif path_values[0] == "groups" and request.method == "GET":
 			return_object["username"] = request.json["username"]
 			return_object["password"] = request.json["password"]
 			return_object["task"] = "GetGroups"
 			return return_object
-		elif path_values[0] == "groups" and path_values[1] == "create":
+		elif path_values[0] == "groups" and request.method == "POST":
 			return_object["username"] = request.json["username"]
 			return_object["password"] = request.json["password"]
 			return_object["groupname"] = request.json["groupname"]
