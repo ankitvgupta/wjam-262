13ff84298b82dc63643f59234b7b92f33c6d29a9
Matthew Rauen <rauen@college.harvard.edu>
2016-02-21 17:51:44 -0500

    Implemented get_messages

--- requestprocessor.py
+++ requestprocessor.py
@@ -8,25 +8,27 @@ class RequestProcessor:
       # userId -> pending messages
       self.messages = {}
 
+    def get_next_id(self, dictionary):
+      if len(dictionary) == 0:
+        return 0
+      return max(dictionary) + 1
+
     def register_user(self, request_object):
-        if len(self.users) == 0:
-          next_id = 0
-        else:
-          next_id = max(self.users) + 1
-        self.users[next_id] = { "username" : request_object["username"], "password" : request_object["password"] }
+        next_user_id                = self.get_next_id(self.users)
+        self.users[next_user_id]    = { "username" : request_object["username"], "password" : request_object["password"] }
+        next_group_id               = self.get_next_id(self.groups)
+        self.groups[next_group_id]  = { "name" : request_object["username"], "users" : [next_user_id] }
+        self.messages[next_user_id] = []
         return { "success" : True, "response" : None }
 
     def list_accounts(self, request_object):
         response = {}
         for user_id in self.users:
-          response[user_id] = self.users["username"]
+          response[user_id] = self.users[user_id]["username"]
         return { "success" : True, "response" : response }
 
     def create_group(self, request_object):
-        if len(self.groups) == 0:
-          next_id = 0
-        else:
-          next_id = max(self.groups) + 1
+        next_id = self.get_next_id(self.groups)
         self.groups[next_id] = { "name" : request_object["name"], "users" : request_object["users"] }
         return { "success" : True, "response" : None }
 
@@ -37,7 +39,14 @@ class RequestProcessor:
         pass
 
     def get_messages(self, request_object):
-        pass
+        user_id  = request_object['user_id']
+
+        if user_id not in self.messages:
+          return { "success" : False, "response" : "User ID does not exist." }
+
+        messages = self.messages[user_id]
+        self.messages[user_id] = []
+        return { "success" : True, "response" : messages }
 
     def delete_account(self, request_object):
         user_id = request_object['user_id']
@@ -45,4 +54,17 @@ class RequestProcessor:
           return { "success" : False, "response" : "User does not exist." }
 
         del self.users[user_id]
+
+        empty_groups = []
+        for group_id in self.groups:
+          if user_id in self.groups[group_id]['users']:
+            self.groups[group_id]['users'].remove(user_id)
+            if len(self.groups[group_id]['users']) == 0:
+              empty_groups.append(group_id)
+
+        for group_id in empty_groups:
+          del self.groups[group_id]
+
+        del self.messages[user_id]
+
         return { "success" : True, "response" : None }
