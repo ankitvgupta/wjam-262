73c129ba9af7c7a6815687aeb9caadc2606ab633
Jonah Kallenbach <jonahkallenbach@jonahs-MacBook-Pro.local>
2016-02-27 03:42:59 -0500

    made things fit nicely

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import sys
 from flask import Flask, render_template, request, jsonify
 from RestAPIStub import RestAPIStub
+from WireProtocolStub import WireProtocolStub
 from requestprocessor import RequestProcessor
 
 app = Flask(__name__)
