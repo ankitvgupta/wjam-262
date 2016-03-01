cc9834dc227d59fe9ef0837e5673d81d25ef02bc
Willy Xiao <willyxiao@gmail.com>
2016-03-01 22:22:46 +0000

    remove comments from server

--- server.py
+++ server.py
@@ -40,24 +40,6 @@ def catch_all(path):
         traceback.print_exc()
         return "failure!" + str(e), 201
 
-# @app.route("/")
-# def hello():
-#     return render_template('index.html')
-
-# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
-# def create_task():
-#     print request
-#     if not request.json or not 'title' in request.json:
-#         abort(400)
-#     task = {
-#         'id': tasks[-1]['id'] + 1,
-#         'title': request.json['title'],
-#         'description': request.json.get('description', ""),
-#         'done': False
-#     }
-#     tasks.append(task)
-#     return jsonify({'task': tasks}), 201
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 30978))
     app.run(host='0.0.0.0', port=port)
