f9f7b1f0f19c1fd81022efeef9f302dec9d78e4b
Willy Xiao <willyxiao@gmail.com>
2016-03-01 04:49:58 +0000

    fixed rest

--- RestAPIStub.py
+++ RestAPIStub.py
@@ -38,8 +38,8 @@ class RestAPIStub:
 		elif path_values[0] == "groups" and request.method == "POST" and len(path_values) == 1:
 			return_object["username"] = request_json["username"]
 			return_object["password"] = request_json["password"]
-			return_object["groupname"] = request_json["groupname"]
-			return_object["groupusers"] = request_json["groupusers"]
+			return_object["name"] = request_json["groupname"]
+			return_object["users"] = request_json["groupusers"]
 			return_object["task"] = "CreateGroup"
 			return return_object
 		elif path_values[0] == "users" and path_values[2] == "messages" and request.method == "GET":
@@ -48,7 +48,7 @@ class RestAPIStub:
 			return_object["task"] = "GetMessages"
 			return return_object
 		elif path_values[0] == "users" and path_values[2] == "messages" and request.method == "POST":
-			return_object["username"] = path_values[1]
+			return_object["username"] = request_json["username"]
 			return_object["password"] = request_json["password"]
 			return_object["message"] = request_json["message"]
 			return_object["groupname"] = path_values[1]
