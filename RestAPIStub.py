013c3cf0a89b0e38e49c23e1dba422b5f46ce9e1
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-03-28 12:24:54 -0400

    updated comments

--- RestAPIStub.py
+++ RestAPIStub.py
@@ -76,33 +76,41 @@ class RestAPIStub:
 		return_object["username"] = request_json['username']
 		return_object["password"] = request_json['password']
 		assert(request_json)
+		# Getting a list of users
 		if path_values[0] == "users" and request.method == "GET" and len(path_values) == 1:
 			return_object["task"] = "ListAccounts"
 			return_object["matchstring"] = request_json["matchstring"]
 			return return_object
+		# Deleting a user account
 		elif path_values[0] == "users" and request.method == "DELETE" and len(path_values) == 1:
 			return_object["task"] = "DeleteAccount"
 			return return_object
+		# Creating a new user
 		elif path_values[0] == "users" and request.method == "POST" and len(path_values) == 1:
 			return_object["task"] = "CreateUser"
 			return return_object
+		# Getting a list of groups
 		elif path_values[0] == "groups" and request.method == "GET" and len(path_values) == 1:
 			return_object["task"] = "GetGroups"
 			return_object["matchstring"] = request_json["matchstring"]
 			return return_object
+		# Creating a group
 		elif path_values[0] == "groups" and request.method == "POST" and len(path_values) == 1:
 			return_object["name"] = request_json["groupname"]
 			return_object["users"] = request_json["groupusers"]
 			return_object["task"] = "CreateGroup"
 			return return_object
+		# Get new messages
 		elif path_values[0] == "users" and path_values[2] == "messages" and request.method == "GET":
 			return_object["task"] = "GetMessages"
 			return return_object
+		# Send a message
 		elif path_values[0] == "users" and path_values[2] == "messages" and request.method == "POST":
 			return_object["message"] = request_json["message"]
 			return_object["groupname"] = path_values[1]
 			return_object["task"] = "SendMessage"
 			return return_object
+		# Invalid request.
 		else:
 			return_object["task"] = "INVALID"
 		return return_object
